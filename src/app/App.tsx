67f1252f8f96d840238682c1893e8dbc754ec2dd
Lam1ro4eQ <alsajgushev@gmail.com>
2024-12-09 16:19:29 +0300

    Loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
 import { Decks } from '../features/decks/Decks.tsx'
 import { GlobalError } from './GlobalError/GlobalError.tsx'
+import { AppRootState, useAppDispatch, useAppSelector } from './store'
+import { useSelector } from 'react-redux'
+import { RequestStatusType } from './app-reducer'
+import { LinearLoader } from '../common/components/Loader/LinearLoader'
 
 export const App = () => {
+  const status = useSelector<AppRootState,RequestStatusType>((state)=> state.app.status)
+  // const status = useAppSelector(state => state.app.status)
   return (
     <div>
+      {status === 'loading' && <LinearLoader/>}
       <Decks />
       <GlobalError />
     </div>
